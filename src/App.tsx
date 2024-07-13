5e8feaad538418adf0beeb8eb5d8ec308c50b3d7
Amir Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-07-13 21:59:13 +0200

    4- Handle correct match

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,34 @@ import Card from "./components/Card/Card";
 
 function App() {
   const [cards, setCards] = useState<CardType[]>([]);
+  const [firstCard, setFirstCard] = useState<number | undefined>();
+  const [secondCard, setSecondCard] = useState<number | undefined>();
+
+  const updateRevealedCards = (index: number) => {
+    if (firstCard !== undefined) {
+      setSecondCard(index);
+    } else {
+      setFirstCard(index);
+    }
+  };
+
+  const reset = () => {
+    setFirstCard(undefined);
+    setSecondCard(undefined);
+  };
+
+  useEffect(() => {
+    if (firstCard !== undefined && secondCard !== undefined) {
+      if (cards[firstCard].src === cards[secondCard].src) {
+        const updatedCards = cards.map((card, i) => ({
+          ...card,
+          revealed: card.revealed || i === firstCard || i === secondCard,
+        }));
+        setCards(updatedCards);
+        reset();
+      }
+    }
+  }, [firstCard, secondCard, cards]);
 
   const initGame = () => {
     const c = shuffle();
@@ -18,7 +46,7 @@ function App() {
     <div className="App">
       <div className="grid">
         {cards.map((card, i) => (
-          <Card key={i} card={card} />
+          <Card key={i} card={card} onClick={() => updateRevealedCards(i)} />
         ))}
       </div>
     </div>
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,16 +3,17 @@ import "./Card.css";
 
 type CardProps = {
   card: CardType;
+  onClick: () => void;
 };
 
-const Card = ({ card }: CardProps) => {
+const Card = ({ card, onClick }: CardProps) => {
   return (
     <div className="card">
       <div className="card__face card__face--front">
         <img src={card.src} alt="card-front" />
       </div>
       <div className="card__face card__face--back">
-        <img src="/pix/back.jpg" alt="card-back" />
+        <img src="/pix/back.jpg" alt="card-back" onClick={onClick} />
       </div>
     </div>
   );
